98ae69d8faf370b594c1775503b1a0697136dfcb
aghiljv <aghiljv@gmail.com>
2020-04-05 15:40:50 +0530

    animated bg added

---
+++ assets/0ee79a39b17454788509.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[5],{201:function(n,t,e){"use strict";e.r(t);var o={},l=e(13),component=Object(l.a)(o,(function(){var n=this.$createElement;return(this._self._c||n)("div",{staticClass:"container"},[this._v("\n\tWorking on Portfolio Content\n")])}),[],!1,null,null,null);t.default=component.exports}}]);
---
+++ assets/5e6f5548f8c34b062b24.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[2],{199:function(n,t,e){"use strict";e.r(t);var o={},l=e(13),component=Object(l.a)(o,(function(){var n=this.$createElement;return(this._self._c||n)("div",{staticClass:"container"},[this._v("\n  Working on Blog Content\n")])}),[],!1,null,null,null);t.default=component.exports}}]);
